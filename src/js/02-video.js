737b53751601251c6ad628efa346c6330b630fca
TetianaKubrak73 <tetianakubrak73@gmail.com>
2023-11-03 15:39:20 +0100

    correct dz-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,14 @@
 import Player from '@vimeo/player';
-    const iframe = document.querySelector('iframe');
-    const player = new Player('vimeo-player');
-    
+
+const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player');
+
 function saveCurrentTime() {
   player.getCurrentTime().then((time) => {
     localStorage.setItem('videoplayer-current-time', time);
   });
 }
+
 function restoreCurrentTime() {
   const currentTime = localStorage.getItem('videoplayer-current-time');
   if (currentTime) {
@@ -15,13 +17,16 @@ function restoreCurrentTime() {
 }
 
 player.on('play', () => {
-  
   saveCurrentTime();
 });
 
 player.on('ended', () => {
-  
   localStorage.removeItem('videoplayer-current-time');
+});
+
+
+player.on('timeupdate', () => {
+  saveCurrentTime();
 });
 
 restoreCurrentTime();
